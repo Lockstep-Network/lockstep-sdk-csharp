b6a4e988605c1ad33726f3734bd438412e98daaf
Ted Spence <tspence@lockstep.io>
2022-03-04 11:16:17 -0800

    Switch to semver3 and add an icon file

--- src/LockstepApi.cs
+++ src/LockstepApi.cs
@@ -8,7 +8,7 @@
  *
  * @author     Ted Spence <tspence@lockstep.io>
  * @copyright  2021-2022 Lockstep, Inc.
- * @version    2022.9.18.0
+ * @version    2022.9.18
  * @link       https://github.com/Lockstep-Network/lockstep-sdk-csharp
  */
 
@@ -39,7 +39,7 @@ namespace LockstepSDK
     {
         // The URL of the environment we will use
         private readonly string _serverUrl;
-        private const string _version = "2022.9.18.0";
+        private const string _version = "2022.9.18";
         private readonly HttpClient _client;
 #if DOT_NET_FRAMEWORK
         private string _appName;
